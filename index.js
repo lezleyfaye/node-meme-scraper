d35aea6f1705439e4b66d3c9e15c8d9d0258d404
Lezley Faye <lezleydavidson@gmail.com>
2023-01-13 16:25:01 +0100

    Add axios code and memes folder

--- index.js
+++ index.js
@@ -3,33 +3,34 @@ import axios from 'axios';
 import cheerio from 'cheerio';
 import fs from 'fs';
 
-// axios.get('/user/12345')
-//    .then(function (response) {
-//  console.log(response.data);
-//  console.log(response.status);
-//  console.log(response.statusText);
-//  console.log(response.headers);
-//  console.log(response.config);
-// });
-
-// downloads html from website
-
+// axios is getting the website and cheerio is loading it
 axios
   .get('https://memegen-link-examples-upleveled.netlify.app/')
   .then((response) => {
     const $ = cheerio.load(response.data);
-    $('img').each((index, element) => {
-      console.log($(element));
+    const memes = [];
+
+    // returns lots of info for each img element
+    $('#images').each((index, element) => {
+      memes.push({
+        url: $(element).find('a').attr('href'),
+      });
+
+      // console.log($(element));
     });
+
+    // created memes.json and stores image url there
+    fs.writeFile('./memes.json', JSON.stringify(memes), (error) => {
+      if (error) throw error;
+    });
+
+    // this is end of the .then response - stop deleting it!
   })
   .catch((error) => {
     console.log(error);
   });
-//
 
-// $('a[href]').each((index, element) => {
-//   console.log($(element).find('a[href]').text().trim());
+// returns list with 0src: 1src: , etc.
+// $('img').each(function (index) {
+//   console.log(index + 'src:' + ' ' + $(this).text());
 // });
-// .catch((error) => {
-//       console.log(error);
-//   });
